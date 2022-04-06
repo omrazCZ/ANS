862524653eff9d7c8ee6fa54a5990713a56f1d7d
hongrchen <hongruyuchen@163.com>
2022-04-06 12:58:14 +0200

    update dataset

--- clean_data.py
+++ clean_data.py
@@ -5,12 +5,16 @@ data = pd.read_csv('ASA All NBA Raw Data.csv')
 print(data.shape)
 
 # Drop unwanted columns
-data.drop(["PG%","SG%","SF%","PF%","C%"], axis=1, inplace=True)
+data.drop(["PG%","SG%","SF%","PF%","C%","last_60_minutes_per_game_bench","last_60_minutes_per_game_starting","active_position_minutes"], axis=1, inplace=True)
 print(data.shape)
 
 # Drop duplicate rows
-data.drop_duplicates(keep='first', inplace=True)
+data.drop_duplicates(subset=['game_id','player_id','player'],keep='first', inplace=True)
 print(data.shape)
+
+# Check cleaned data
+#test = data.groupby(['game_id'])['starter'].sum().reset_index()
+#test.to_csv('test_NBA_Clean_Data.csv', index=False, header=True)
 
 # Save cleaned data
 data.to_csv('NBA_Clean_Data.csv', index=False, header=True)
