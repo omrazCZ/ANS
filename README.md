68161c0bd07688f9a42c988bc70f554fdd612b75
omrazCZ <oto.mraz@gmail.com>
2022-04-07 15:55:19 +0200

    Add soccer data

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 Predicting performance of basketball teams
 
 The dataset can be found at https://www.kaggle.com/datasets/wyattowalsh/basketball as part of a Kaggle Challenge. We provide the data tables transformed into csv files.
+
+Soccer dataset available at https://www.kaggle.com/datasets/hugomathien/soccer as part of a challenge.
+
+Some of the files are too large, therefore we provide a script convert_data.py to generate them localy (assuming you have dowloaded the sqlite files from Kaggle). E.g.
+
+```python convert_data.py archive/database.sqlite soccer_data```
--- convert_data.py
+++ convert_data.py
@@ -1,8 +1,17 @@
+import imp
 import numpy as np
 import pandas as pd
 import sqlite3
+import sys
+import os
+from os.path import isdir, join
 
-db_file = 'archive/basketball.sqlite'
+db_file = sys.argv[1]
+dest_folder = sys.argv[2]
+
+#db_file = 'archive/basketball.sqlite'
+#dest_folder = 'basketball_data/'
+
 # Read DB file
 con = sqlite3.connect(db_file, isolation_level=None,
                       detect_types=sqlite3.PARSE_COLNAMES)
@@ -15,8 +24,11 @@ tables = [i[0] for i in t]
 print(tables)
 
 # Store data in csv files
+if not isdir(join(os.getcwd(), dest_folder)):
+    os.mkdir(dest_folder)
+
 for table in tables:
     cur = pd.read_sql_query('SELECT * FROM ' + table, con)
-    cur.to_csv('basketball_data/' + table + '.csv', index=False)
+    cur.to_csv(join(dest_folder, table + '.csv'), index=False)
 
 print("Done")
